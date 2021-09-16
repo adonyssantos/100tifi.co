ecdf82a7af220982541258b823edcac1a5e267cd
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-09-16 19:05:47 -0400

    Add Templates

---
+++ src/pages/Error404.js
@@ -0,0 +1,12 @@
+const Error404 = () => {
+  const view = /*html*/ `
+	  <div class="Error404">
+	    <h2>404</h2>
+		<p>Page not found</p>
+	  </div>
+	  `;
+
+  return view;
+};
+
+export default Error404;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+  const view = /* html */ `
+	  <div class="Characters">
+	  	<article class="Character-item">
+		  <a href="#/character/1">
+		    <img src="image" alt="name" />
+			h2>Character 1</h2>
+		  </a>
+		</article>
+	  </div>
+	`;
+  return view;
+};
+
+export default Home;
